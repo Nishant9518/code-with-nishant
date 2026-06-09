fa970d4ba4b77ffb3b32388605e19d64553a4557
Nishant9518 <sharmaji9518220885@gmail.com>
2026-06-09 11:18:33 +0530

    variables

--- test.js
+++ test.js
@@ -1,1 +1,24 @@
-let a = prompt("enter a name");
+// Temporal Dead Zone (TDZ) example
+console.log(a);
+var a = 10;
+
+// Output:
+// undefined
+
+// Hoisting impact pert type
+var a = 10; // ye do hisso me tut jata
+var a = undefined;
+a = 10;
+console.log(nm);
+var nm ="Nishant";
+var x = 5;
+{
+    var x = 10; // var is function-scoped, so it will affect the outer variable
+}
+console.log(x); // Output: 10
+let n = 4;
+{
+    let n = 8; // let is block-scoped, so it will not affect the outer variable
+    console.log(n); // Output: 8
+}
+console.log(n); // Output: 4
