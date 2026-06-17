dbf32d89a191bef39cb6e5ae2cacf0fe9be7a246
Nishant9518 <sharmaji9518220885@gmail.com>
2026-06-17 22:26:31 +0530

    l

---
+++ cond.js
@@ -0,0 +1,15 @@
+// if else elseif 
+// switch case
+// early return pattern
+switch (3) {
+    case 1 :
+        console.log("one");
+        break;
+    case 2 :
+        console.log("two");
+        break;
+    case 3 :
+        console.log("three");
+        break;
+    default:
+}
