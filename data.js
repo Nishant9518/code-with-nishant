76b0fe9392bec666efdf9e27cd6d99a422badf78
Nishant9518 <sharmaji9518220885@gmail.com>
2026-06-17 16:21:32 +0530

    type

--- data.js
+++ data.js
@@ -2,6 +2,14 @@
 //let b = a 
 //b.pop();
 //console.log(a);
-let u = Symbol("001");
+/*let u = Symbol("001");
 let v = Symbol("001");
-console.log(u == v);
+console.log(u == v);*/
+//console.log(typeof null)
+//console.log(typeof undefined   )
+//console.log(typeof NaN)
+// type coercion -> jisme ek type dusre type me automatically convert ho jat hai
+let a = 10;
+let b = "20";
+console.log(a + b ) // this also called concateniantion
+
