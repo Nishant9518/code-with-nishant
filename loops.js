9e010eba65897e4c3d352150c6beaaba24052dea
Nishant9518 <sharmaji9518220885@gmail.com>
2026-06-28 20:40:17 +0530

    start

---
+++ loops.js
@@ -0,0 +1,11 @@
+// loops
+// for loop jb pta ho kha jana h kaise jana or kb tk jana<<
+// while loop kha tk jana h , kb rukna h , kaise jana h  
+/*for (let i = 1; i<=100; i++) {
+    console.log(i);
+} */
+let i = 1;
+while (i <= 32){
+    console.log(i);
+    i++;
+}
