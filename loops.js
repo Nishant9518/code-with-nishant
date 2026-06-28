35f40dc7816d827776726c70d3f874c01eda5c22
Nishant9518 <sharmaji9518220885@gmail.com>
2026-06-28 21:45:32 +0530

    end

--- loops.js
+++ loops.js
@@ -3,9 +3,31 @@
 // while loop kha tk jana h , kb rukna h , kaise jana h  
 /*for (let i = 1; i<=100; i++) {
     console.log(i);
-} */
+} 
 let i = 1;
 while (i <= 32){
     console.log(i);
     i++;
+} */
+// print number 1 to 10 using for loop
+//for (let i = 1; i <= 10; i++) {
+  //  console.log(i);
+//}
+// print number 10 to 1 using while loop
+let i = 10;
+while(i>=1){
+    console.log(i)
+    i--
+}
+for(let i = 1; i<=20; i++){
+    if(i%2==0){
+        console.log(i)
+    }
+}
+let j = 1;
+while(j<=15){
+    if(j%2!=0){
+        console.log(j)
+    }
+    j++
 }
